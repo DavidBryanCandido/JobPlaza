630b4eeec091bd39b30f6ef759c7035e93ca7ea5
DavidBryanCandido <202011090@gordoncollege.edu.ph>
2023-05-27 14:35:31 +0800

    minor last minute update

--- resources/views/includes/apply-form.blade.php
+++ resources/views/includes/apply-form.blade.php
@@ -1,20 +1,41 @@
-<form action="{{ route('job.apply', $job->id) }}" method="POST" enctype="multipart/form-data">
-
-    @csrf
-    <div class="form-group">
-        <label for="name">Name</label>
-        <input type="text" name="name" id="name" required>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            {{-- CSRF Token --}}
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
+    <title>apply-form</title>
+        <link href="{{ asset('css/apply.css') }}" type="text/css" rel="stylesheet"/>
+</head>
+<body>
+    <div class="container">
+        
+        <div class="concon">
+            <div class="back">
+                <a href="{{ route('jobs.index') }}"><img src="/img/close.png" alt="close"></a>
+            </div>
+            <form action="{{ route('job.apply', $job->id) }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name" required>
+                </div>
+    
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" required>
+                </div>
+    
+                <div class="form-group g">
+                    <label for="resume">Resume</label>
+                    <input type="file" name="resume" id="resume" required>
+                </div>
+    
+                <button class="btn" type="submit">Submit</button>
+            </form>        
+        </div>
     </div>
-
-    <div class="form-group">
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email" required>
-    </div>
-
-    <div class="form-group">
-        <label for="resume">Resume</label>
-        <input type="file" name="resume" id="resume" required>
-    </div>
-
-    <button type="submit">Submit</button>
-</form>
+</body>
+</html>
